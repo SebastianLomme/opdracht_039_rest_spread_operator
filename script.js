c28c0134d8bb21bbc5dd956ad862051511a13a4a
Sebastian Lomme <sebastian.lomme@gmail.com>
2021-08-17 11:35:24 +0200

    opdracht klaar

---
+++ script.js
@@ -0,0 +1,11 @@
+const addAll = (...num) => num.reduce((prev, current) => prev + current);
+
+
+console.log(addAll(1,2,3,4));
+
+const Optellen = (num1, num2, num3) => sum = num1 + num2 + num3;
+
+
+const arrayNumber = [1,2,3];
+
+console.log(Optellen(...arrayNumber));
